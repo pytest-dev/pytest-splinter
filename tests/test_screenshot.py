c5cb90d28f55acb493cf20fa1ce1bdf0b91fad72
Daniel Hahler <git@thequod.de>
2015-01-29 18:17:34 +0100

    test_browser_screenshot_error: patch '_pytest.config.Config.warn'

--- tests/test_screenshot.py
+++ tests/test_screenshot.py
@@ -1,6 +1,8 @@
 """Browser screenshot tests."""
 import pytest
 
+from mock import patch
+
 
 def test_browser_screenshot_normal(testdir, mocked_browser):
     """Test making screenshots on test failure if the commandline option is passed.
@@ -15,24 +17,18 @@ def test_browser_screenshot_normal(testdir, mocked_browser):
     assert testdir.tmpdir.join('test_browser_screenshot_normal', 'test_screenshot-browser.png').isfile()
 
 
-def test_browser_screenshot_error(testdir, mocked_browser):
+@patch('_pytest.config.Config.warn')
+def test_browser_screenshot_error(testdir, mocked_browser, request):
     """Test warning with error during taking screenshots on test failure."""
-    testdir.makepyfile("""
+    testdir.inline_runsource("""
         def test_screenshot(browser):
             # Create a file here, so makedirs in make_screenshot_on_failure will fail.
             open('isafile', 'w').close()
             assert False
-    """)
-    result = testdir.runpytest('-sv', "--splinter-screenshot-dir=isafile",
-                               '-rw')
 
-    result.stdout.fnmatch_lines([
-        '*test_screenshot FAILED*',
-        'Wsplinter None Could not save screenshot: * Not a directory*'
-    ])
-    assert '*test_screenshot ERROR*' not in result.stdout.str()
-
-    assert result.ret != 0
+        def test_warn_called(request):
+            assert request.config.warn.call_count == 1
+    """, "-vl", "--splinter-session-scoped-browser=false")
 
 
 @pytest.mark.skipif('not config.pluginmanager.getplugin("xdist")', reason='pytest-xdist is not installed')
