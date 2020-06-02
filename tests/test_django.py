f64cedd4ce5f81aca9c68a22f5b2be3d145d929b
Michel Sabchuk <michel@turbosys.com.br>
2020-06-02 19:35:47 -0300

    Try to access the django-admin interface using django driver.
    If we reach admin, we can assume django is working!

---
+++ tests/test_django.py
@@ -0,0 +1,11 @@
+"""Tests for pytest-splinter django driver availability."""
+import pytest
+
+
+@pytest.mark.parametrize("splinter_webdriver", ["django"])
+def test_django_client(splinter_webdriver, browser):
+    """
+    Test the availability of django driver by testing hit the admin.
+    """
+    browser.visit("/admin/")
+    assert browser.is_text_present("Django administration")
