5889057d1822f105d0a939af2c0e2868d3a572b8
Michel Sabchuk <michel@turbosys.com.br>
2020-06-02 20:28:09 -0300

    Run django test separated from other tests.
    This way, we can opt out of pytest-django.

--- tests/test_django.py
+++ tests/test_django.py
@@ -3,6 +3,7 @@ import pytest
 
 
 @pytest.mark.parametrize("splinter_webdriver", ["django"])
+@pytest.mark.django
 def test_django_client(splinter_webdriver, browser):
     """
     Test the availability of django driver by testing hit the admin.
