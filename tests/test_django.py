93b1712dbb00ddabf6a6c6210da3fef85b1a66e9
Michel Sabchuk <michel@turbosys.com.br>
2020-06-05 15:58:13 -0300

    Abstract splinter browser through a mocked browser.

--- tests/test_django.py
+++ tests/test_django.py
@@ -1,12 +1,30 @@
 """Tests for pytest-splinter django driver availability."""
 import pytest
+import mock
 
 
-@pytest.mark.parametrize("splinter_webdriver", ["django"])
-@pytest.mark.django
-def test_django_client(splinter_webdriver, browser):
+@pytest.fixture(autouse=True)
+def driverless_browser(request):
     """
-    Test the availability of django driver by testing hit the admin.
+    Mock splinter browser specifically for driverless browsers.
+
+    Django and Flask browsers extends the LxmlDriver and doesn't provide a
+    driver attribute.
     """
-    browser.visit("/admin/")
-    assert browser.is_text_present("Django administration")
+
+    def mocked_browser(driver_name, *args, **kwargs):
+        mocked_browser = mock.Mock()
+        del mocked_browser.driver  # force AttributeError
+        mocked_browser.driver_name = driver_name
+        mocked_browser.is_text_present.return_value = True
+        return mocked_browser
+
+    with mock.patch("pytest_splinter.plugin.splinter.Browser", mocked_browser):
+        yield
+
+
+@pytest.mark.parametrize("splinter_webdriver", ["django", "flask"])
+def test_driverless_splinter_browsers(splinter_webdriver, browser):
+    """Test the driverless splinter browsers django and flask."""
+    browser.visit("/")
+    assert browser.is_text_present("Ok!") is True
