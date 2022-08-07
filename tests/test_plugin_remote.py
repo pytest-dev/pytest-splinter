f5f308fcf4cc5a99690672c77ee3fe673e171972
Michał Pasternak <michal.dtz@gmail.com>
2022-08-07 10:14:54 +0200

    Never commit a line that runs debugger

--- tests/test_plugin_remote.py
+++ tests/test_plugin_remote.py
@@ -52,5 +52,4 @@ def test_keep_alive(simple_page, browser, splinter_webdriver):
     """Test that Remote WebDriver keep_alive is True."""
     if splinter_webdriver != "remote":
         pytest.skip("Only Remote WebDriver uses keep_alive argument")
-    import pytest; pytest.set_trace()
     assert browser.driver.keep_alive
