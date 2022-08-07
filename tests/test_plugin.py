ea78300053a0a68c61fab43651396f630fa9fc33
Michał Pasternak <michal.dtz@gmail.com>
2022-08-07 09:49:41 +0200

    Fix CI

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -10,21 +10,6 @@ from pytest_splinter.plugin import get_args
 
 
 @pytest.fixture
-def simple_page_content():
-    """Return simple page content."""
-    return """<html xmlns="http://www.w3.org/1999/xhtml"><head></head>
-    <body>
-        <div id="content">
-            <p>
-                Some <strong>text</strong>
-            </p>
-        </div>
-        <textarea id="textarea">area text</textarea>
-    </body>
-</html>"""
-
-
-@pytest.fixture
 def simple_page(httpserver, browser, simple_page_content):
     """Serve simple html page."""
     httpserver.serve_content(
